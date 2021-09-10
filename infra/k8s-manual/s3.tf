5fc64e59c991342e2da5de29cfb3eee4423cb4fb
Eloy Vega <eloy.vega@gmail.com>
2021-09-10 10:17:01 -0500

    Add output for kubeconfig

--- infra/k8s-manual/s3.tf
+++ infra/k8s-manual/s3.tf
@@ -3,3 +3,7 @@ resource "aws_s3_bucket" "kubeadm_bucket" {
   acl           = "private"
   force_destroy = true
 }
+
+output "kubeconf_download" {
+  value = "aws s3 cp s3://${aws_s3_bucket.kubeadm_bucket.id}/config ~/.kube/config"
+}
